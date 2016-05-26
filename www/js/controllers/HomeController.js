c11bcac9d4fb2851ed5bd035f76f9d4b2f84df7f
Matthew Bouchard <bouchard.matthewj@gmail.com>
2016-05-25 20:35:59 -0600

    commit before pulling

--- www/js/controllers/HomeController.js
+++ www/js/controllers/HomeController.js
@@ -1,8 +1,8 @@
 (function() {
   angular.module('starter')
 
-  .controller('HomeCtrl', function($scope, $log){
-
+  .controller('HomeCtrl', function($scope, $log, theUser){
+    $log.info('currentuser====',theUser)
     $scope.doItLive = function() {
       console.log('live bitchessssss');
       $log.info('hi');
