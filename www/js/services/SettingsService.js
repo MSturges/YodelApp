5f86a2611115cce39eaa933dc301d407c377861f
James Freeman <freeman.james.h@gmail.com>
2016-05-26 18:14:54 -0600

    changed post route to /settingsupdate

--- www/js/services/SettingsService.js
+++ www/js/services/SettingsService.js
@@ -6,7 +6,7 @@ return {
     updateInterests: function (settingsObj){
       console.log('In service: ', settingsObj)
       var deferred = $q.defer();
-      $http.post('http://yodelappbcjmm.herokuapp.com/signup', settingsObj)
+      $http.post('http://yodelappbcjmm.herokuapp.com/settingsupdate', settingsObj)
       .then(function(success){
         deferred.resolve(success)
       })
