c3cf4cb6d5f78698166d4bb6919fc5262f6badeb
Matthew Bouchard <bouchard.matthewj@gmail.com>
2016-05-27 12:39:03 -0600

    yodel is yodeling

--- www/js/services/TabService.js
+++ www/js/services/TabService.js
@@ -1,10 +1,9 @@
 angular.module('starter')
 
 .factory('TabService', function ($http, $p, $log, ngAudio) {
-  $scope.sound = ngAudio.load('http://www.archive.org/download/SwissYodelCall/Track17_64kb.m3u');
-  play('http://www.archive.org/download/SwissYodelCall/Track17_64kb.mp3')
   return {
     go: function (userObj){
+
       var deferred = $q.defer();
       ngAudio.play($scope.sound)
       .then(function(){
